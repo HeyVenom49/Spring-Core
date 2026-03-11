ce0aceee5c28b108dd71308bf4db0a3761a9d2f5
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-03-11 12:31:29 +0530

    How to know which collection we're using to store

--- src/main/java/com/springcore/collections/Test.java
+++ src/main/java/com/springcore/collections/Test.java
@@ -4,9 +4,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-       ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
-       Emp emp1 = (Emp) context.getBean("emp1");
-       System.out.println(emp1);
-       context.close();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "com/springcore/collections/collectionconfig.xml");
+        Emp emp1 = (Emp) context.getBean("emp1");
+        System.out.println(emp1);
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getAddresses());
+        System.out.println(emp1.getCourses().getClass().getName());
+        System.out.println(emp1.getPhoneNumber().getClass().getName());
+        context.close();
     }
 }
